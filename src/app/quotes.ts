bfee45cdcc937008e2bd4d60818d7c94dd545583
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-07 02:11:18 +0300

    add changes made to quotes.ts on constructor

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,8 +1,6 @@
 export class Quotes {
-    hasAuthor: boolean;
-    hasPerson: boolean;
+   
     constructor(public quote: string, public author: string, public person: string){
-      this.hasAuthor=false;
-      this.hasPerson=false;
+     
     }
   }
