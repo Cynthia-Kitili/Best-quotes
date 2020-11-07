9dc4f10b5f09f52782c68e6f65831c80e8cc4a55
Cynthia Kitili <nyambucindy0@gmail.com>
2020-11-08 00:52:22 +0300

    add changes made to form

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
+  constructor(private elem: ElementRef) { }
 
-  constructor(private elem:ElementRef) { }
-
+  @HostListener("click") quote() {
+    this.textDeco("highlight")
+    
+  }
+  
+  private textDeco(action: string){
+    this.elem.nativeElement.style.textDecoration = 'action';
+  }
 }
+
+
